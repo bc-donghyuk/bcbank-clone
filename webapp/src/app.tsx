56d0be6ee8a603a2d4ab93aaefed112a3e935662
piouy_ <piouy_@blockcrafters.com>
2022-03-21 23:01:11 +0900

    Update react router v6

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
+import { Routes, Route } from "react-router-dom";
+
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+
 const App = () => {
   return (
-    <div>
-      <div>Hello world</div>
-    </div>
+    <Routes>
+      <Route path="/" element={<div>Hello world</div>} />
+      <Route path="about" element={<div>Hello about</div>} />
+    </Routes>
   );
 };
 
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "app";
 
@@ -8,4 +9,9 @@ import "css/global.scss";
 
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />, rootElement);
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement,
+);
