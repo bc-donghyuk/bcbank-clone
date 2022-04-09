82a35293cf85245b8cdc1f691271a31959b6b3b2
piouy_ <piouy_@blockcrafters.com>
2022-04-09 14:23:01 +0900

    Update MuiThemeProvider

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1,40 +1,44 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import smil from "modernizr-esm/feature/svg/smil";
+import { ThemeProvider } from "@material-ui/core";
 
 import DesktopWrapper from "components/common/desktop/DesktopWrapper";
 import UnsupportBrowserPage from "components/unsupport/UnsupportBrowserPage";
 
 import { routes, routeComponents } from "routes/routeConfig";
 import { LOGIN_URL } from "URLConstant";
+import MuiTheme from "styles/theme";
 
 const App = () => {
   if (smil) {
     return (
-      <Routes>
-        {
-          // isLoggedIn()?
-          <Route path="/" element={<Navigate replace to={LOGIN_URL} />} />
-        }
-        {routes.map(route => {
-          const RouteComponent = routeComponents[route.type];
+      <ThemeProvider theme={MuiTheme}>
+        <Routes>
+          {
+            // isLoggedIn()?
+            <Route path="/" element={<Navigate replace to={LOGIN_URL} />} />
+          }
+          {routes.map(route => {
+            const RouteComponent = routeComponents[route.type];
 
-          return (
-            <RouteComponent
-              key={route.path}
-              path={route.path}
-              element={(() => {
-                const node = <route.component />;
-                if (route.fullscreen) {
-                  return node;
-                }
-                return <DesktopWrapper>{node}</DesktopWrapper>;
-              })()}
-              {...route.routeProps}
-            />
-          );
-        })}
-      </Routes>
+            return (
+              <RouteComponent
+                key={route.path}
+                path={route.path}
+                element={(() => {
+                  const node = <route.component />;
+                  if (route.fullscreen) {
+                    return node;
+                  }
+                  return <DesktopWrapper>{node}</DesktopWrapper>;
+                })()}
+                {...route.routeProps}
+              />
+            );
+          })}
+        </Routes>
+      </ThemeProvider>
     );
   }
   return <UnsupportBrowserPage />;
---
+++ webapp/src/styles/theme.js
@@ -0,0 +1,38 @@
+import { createTheme } from "@material-ui/core";
+
+import colors from "./colors";
+
+const MuiTheme = createTheme({
+  breakpoints: {
+    values: {
+      sm: 375,
+      md: 768,
+      lg: 1280,
+    },
+  },
+  palette: {
+    primary: { main: colors.brand["100"] },
+  },
+  status: {
+    danger: "orange",
+  },
+  typography: {
+    fontFamily: "Roboto",
+  },
+  overrides: {
+    MuiExpansionPanelSummary: {
+      root: {
+        "&$expanded": {
+          minHeight: 48,
+        },
+      },
+      content: {
+        "&$expanded": {
+          margin: "12px 0",
+        },
+      },
+    },
+  },
+});
+
+export default MuiTheme;
