48c77d79e1dea65ccfc4fa704d21a036ad82f7eb
piouy_ <piouy_@blockcrafters.com>
2022-05-06 20:29:58 +0900

    WIP Signup hook

--- webapp/src/hooks/auth/useLogin.ts
+++ webapp/src/hooks/auth/useLogin.ts
@@ -1,4 +1,3 @@
-import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { FieldValues, UseFormSetValue } from "react-hook-form";
---
+++ webapp/src/hooks/auth/useSignup.ts
@@ -0,0 +1,7 @@
+const useSignup = () => {
+  const signup = () => {};
+
+  return { signup };
+};
+
+export default useSignup;
