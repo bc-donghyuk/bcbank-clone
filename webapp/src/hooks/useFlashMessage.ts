01160eecc6903f8aa18baea38982cbd7eb9a20c8
piouy_ <piouy_@blockcrafters.com>
2022-05-11 18:34:31 +0900

    WIP Update flashMessage

--- webapp/src/hooks/useFlashMessage.ts
+++ webapp/src/hooks/useFlashMessage.ts
@@ -3,16 +3,24 @@ import { useCallback } from "react";
 import { useRecoilState } from "recoil";
 import { flashMessageState as $flashMessageState } from "recoil/atoms/flashMessage";
 
+interface message {
+  type: "addMessage";
+  body: React.FC<any> | (() => React.ReactNode) | string;
+  targetUrl: string;
+  messageType?: number;
+  customIcon?: React.ReactNode;
+}
+
 const useFlashMessage = () => {
   const [flashMessageState, setFlashMessageState] = useRecoilState($flashMessageState);
 
-  const addMessage = useCallback(() => {
+  const addMessage = ({ body, targetUrl, type, customIcon }:message) => useCallback(() => {
     setFlashMessageState(
       produce(draft => {
-        // draft.message =
+        // draft.message = new Message({})
       }),
     );
-  }, [setFlashMessageState]);
+  }, [setFlashMessageState]));
 
   return { addMessage };
 };
